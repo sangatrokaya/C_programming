54b8e70cba39e8ffac0a67e53dd096f37ae36461
sangatification <rokayasangat123@gmail.com>
2024-02-19 14:48:09 +0545

    cartesian to polar coordinate where theta is in degree

---
+++ Basic C programs/cartesian_to_polar_coordinates.c
@@ -0,0 +1,30 @@
+// WAP to convert cartesian coordinates to polar coordinates
+
+/*
+To convert from Cartesian Coordinates (x,y) to Polar Coordinates (r,θ):
+r = √ ( x2 + y2 )
+θ = tan-1 ( y / x )
+*/
+#include<stdio.h>
+#include<math.h>
+#define PI 3.141592
+
+int main () 
+{
+    float x, y, r, theta;
+    printf("Enter cartesian coordinate x: ");
+    scanf("%f", &x);
+    printf("Enter cartesian coordinate y: ");
+    scanf("%f", &y);
+
+    /* Calculating r */
+    r = sqrt (x *x + y*y);
+    /* calculating theta in radian */
+    theta = atan (y/x);
+    /* Converting theta from degree to radian */
+    theta = 180.0 * theta / PI;
+
+    printf("Polar coordinate is: (%0.2f, %0.2f)", r,theta);
+
+    return 0;
+}
