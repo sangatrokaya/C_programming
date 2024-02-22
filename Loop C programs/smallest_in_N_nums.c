676886fcafc8f6071ef1706dbd9e12c17e7907a2
sangatification <rokayasangat123@gmail.com>
2024-02-22 15:47:34 +0545

    smallest number among N numbers

---
+++ Loop C programs/smallest_in_N_nums.c
@@ -0,0 +1,26 @@
+// C program to find smallest among N numbers
+#include<stdio.h>
+int main ()
+{
+    int n, i;
+    float s,  num;
+    printf("How many numbers? : \n");
+    scanf("%d", &n);
+    printf("Enter number-1 : ");
+    scanf("%f", &num);
+
+    s = num;   /* Assume the first number is the smallest */
+    i = 2;
+    while (i <= n)
+    {
+        printf("Enter number-%d : ", i);
+        scanf("%f", &num);
+        if (num < s)
+        {
+            s = num;
+        }
+        i++;
+    }
+    printf("Smallest number is %0.2f", s);
+    return 0;
+}
