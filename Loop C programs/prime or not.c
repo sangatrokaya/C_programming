2b25a46c44448e6969eb6283647442ae55d1553e
sangatification <timetosangatify66@gmail.com>
2024-02-25 14:39:20 +0545

    readme file added

---
+++ Loop C programs/prime or not.c
@@ -0,0 +1,11 @@
+// C program to check whether a given number is prime or not
+// A Prime Number is a positive integer greater than 1 which is divisible by 1
+#include<stdio.h>
+int main ()
+{
+    int n, i;
+    printf("Enter an integer number: ");
+    scanf("%d", &n);
+
+    for (i=2; i<=n;)
+}
